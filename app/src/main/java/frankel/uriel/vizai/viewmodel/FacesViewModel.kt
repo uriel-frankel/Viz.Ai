ca1e31a68da74a8453e2a65a4b24b9cd2f74cdc8
Uriel Frankel <uriel@houzz.com>
2019-11-21 15:35:31 +0200

    clean code

--- app/src/main/java/frankel/uriel/vizai/viewmodel/FacesViewModel.kt
+++ app/src/main/java/frankel/uriel/vizai/viewmodel/FacesViewModel.kt
@@ -6,12 +6,11 @@ import androidx.lifecycle.ViewModel
 import frankel.uriel.vizai.model.network.ApiService
 import frankel.uriel.vizai.utils.Resource
 import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.asRequestBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.io.File
-import java.io.FileInputStream
 
 class FacesViewModel : ViewModel() {
 
@@ -21,14 +20,10 @@ class FacesViewModel : ViewModel() {
 
     fun sendImageToServer(file: File) {
         emotion.postValue(Resource.Loading())
-        val inputStream = FileInputStream(file)
-        val buf = ByteArray(inputStream.available())
-        while (inputStream.read(buf) !== -1);
-        val requestBodyByte = RequestBody
-            .create("application/octet-stream".toMediaType(), buf)
-        val content_disposition =
-            "attachment; filename=\"" + file.path + "\""
-        ApiService.instance.service.detectFaces(content_disposition, requestBodyByte)
+
+        val requestBody = file.asRequestBody("application/octet-stream".toMediaType())
+        val contentDisposition =  "attachment; filename=\"" + file.path + "\""
+        ApiService.instance.service.detectFaces(contentDisposition, requestBody)
             ?.enqueue(object : Callback<Array<Face>?> {
                 override fun onFailure(call: Call<Array<Face>?>, t: Throwable) {
 
